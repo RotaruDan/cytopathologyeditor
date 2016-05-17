1386276f5e8b2989e9897a8fd12d9c557850dae0
RotaruDan <drotaru@ucm.es>
2016-05-17 12:02:10 +0200

    Mongo host

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/material-dev',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/material-dev',
 	app: {
 		title: 'Cytopathology Challenge Editor - Development Environment'
 	},
