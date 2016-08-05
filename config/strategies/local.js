1b60266ede33027376705dcd533b03eca0a9aea0
RotaruDan <drotaru@ucm.es>
2016-08-05 14:29:36 +0200

    BasicStrategy authentication

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -4,35 +4,59 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	LocalStrategy = require('passport-local').Strategy,
-	User = require('mongoose').model('User');
+    LocalStrategy = require('passport-local').Strategy,
+    BasicStrategy = require('passport-http').BasicStrategy,
+    User = require('mongoose').model('User');
 
-module.exports = function() {
-	// Use local strategy
-	passport.use(new LocalStrategy({
-			usernameField: 'username',
-			passwordField: 'password'
-		},
-		function(username, password, done) {
-			User.findOne({
-				username: username
-			}, function(err, user) {
-				if (err) {
-					return done(err);
-				}
-				if (!user) {
-					return done(null, false, {
-						message: 'Unknown user or invalid password'
-					});
-				}
-				if (!user.authenticate(password)) {
-					return done(null, false, {
-						message: 'Unknown user or invalid password'
-					});
-				}
+module.exports = function () {
+    // Use local strategy
+    passport.use(new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password'
+        },
+        function (username, password, done) {
+            User.findOne({
+                username: username
+            }, function (err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    return done(null, false, {
+                        message: 'Unknown user or invalid password'
+                    });
+                }
+                if (!user.authenticate(password)) {
+                    return done(null, false, {
+                        message: 'Unknown user or invalid password'
+                    });
+                }
 
-				return done(null, user);
-			});
-		}
-	));
+                return done(null, user);
+            });
+        }
+    ));
+
+    passport.use(new BasicStrategy(
+        function (userid, password, done) {
+            User.findOne({username: userid}, function (err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    return done(null, false, {
+                        message: 'Unknown user or invalid password'
+                    });
+                }
+                if (!user.authenticate(password)) {
+                    return done(null, false, {
+                        message: 'Unknown user or invalid password'
+                    });
+                }
+
+                return done(null, user);
+            });
+        }
+    ));
+
 };
