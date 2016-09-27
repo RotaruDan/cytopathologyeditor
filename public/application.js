d426847a7eafefde9bb967842129aef9801b87ca
RotaruDan <drotaru@ucm.es>
2016-09-27 22:42:28 +0200

    package and Bower.json

--- public/application.js
+++ public/application.js
@@ -13,30 +13,9 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
 //Set up the color theme for the application https://material.angularjs.org/latest/#/Theming/01_introduction
 angular.module(ApplicationConfiguration.applicationModuleName)
     .config(function ($mdThemingProvider) {
-        $mdThemingProvider.definePalette('amazingPaletteName', {
-            '50': 'ffffff',
-            '100': 'bbdefb',
-            '200': '90caf9',
-            '300': '64b5f6',
-            '400': '42a5f5',
-            '500': '2196f3',
-            '600': '1e88e5',
-            '700': '1976d2',
-            '800': '1565c0',
-            '900': '0d47a1',
-            'A100': '82b1ff',
-            'A200': '448aff',
-            'A400': '2979ff',
-            'A700': '2962ff',
-            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-                                                // on this palette should be dark or light
-            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-                '200', '300', '400', 'A100'],
-            'contrastLightColors': undefined    // could also specify this if default was 'dark'
-        });
         $mdThemingProvider.theme('default')
             //Custom background palette
-            .backgroundPalette('amazingPaletteName', {
+            .backgroundPalette('light-blue', {
                 'default': '50'
             })
             .primaryPalette('blue')
