e67ef87aabe4aa4d6461a6d8a7cd009e9b146775
RotaruDan <drotaru@ucm.es>
2016-09-29 20:43:59 +0200

    Fixes Facebook login error, improves github and twitter

--- config/strategies/facebook.js
+++ config/strategies/facebook.js
@@ -4,39 +4,53 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	url = require('url'),
-	FacebookStrategy = require('passport-facebook').Strategy,
-	config = require('../config'),
-	users = require('../../app/controllers/users.server.controller');
+    url = require('url'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    config = require('../config'),
+    users = require('../../app/controllers/users.server.controller');
 
-module.exports = function() {
-	// Use facebook strategy
-	passport.use(new FacebookStrategy({
-			clientID: config.facebook.clientID,
-			clientSecret: config.facebook.clientSecret,
-			callbackURL: config.facebook.callbackURL,
-			passReqToCallback: true
-		},
-		function(req, accessToken, refreshToken, profile, done) {
-			// Set the provider data and include tokens
-			var providerData = profile._json;
-			providerData.accessToken = accessToken;
-			providerData.refreshToken = refreshToken;
+module.exports = function () {
+    // Use facebook strategy
+    passport.use(new FacebookStrategy({
+            clientID: config.facebook.clientID,
+            clientSecret: config.facebook.clientSecret,
+            callbackURL: config.facebook.callbackURL,
+            profileFields: ['id', 'name', 'displayName', 'emails', 'photos'],
+            passReqToCallback: true
+        },
+        function (req, accessToken, refreshToken, profile, done) {
+            // Set the provider data and include tokens
+            var providerData = profile._json;
+            providerData.accessToken = accessToken;
+            providerData.refreshToken = refreshToken;
 
-			// Create the user OAuth profile
-			var providerUserProfile = {
-				firstName: profile.name.givenName,
-				lastName: profile.name.familyName,
-				displayName: profile.displayName,
-				email: profile.emails[0].value,
-				username: profile.username,
-				provider: 'facebook',
-				providerIdentifierField: 'id',
-				providerData: providerData
-			};
+            // Create the user OAuth profile
+            var providerUserProfile = {
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                displayName: profile.displayName,
+                email: profile.emails ? profile.emails[0].value : undefined,
+                username: profile.username || generateUsername(profile),
+                profileImageURL: (profile.id) ? '//graph.facebook.com/' + profile.id + '/picture?type=large' : undefined,
+                provider: 'facebook',
+                providerIdentifierField: 'id',
+                providerData: providerData
+            };
 
-			// Save the user OAuth profile
-			users.saveOAuthUserProfile(req, providerUserProfile, done);
-		}
-	));
+            // Save the user OAuth profile
+            users.saveOAuthUserProfile(req, providerUserProfile, done);
+
+            function generateUsername(profile) {
+                var username = '';
+
+                if (profile.emails) {
+                    username = profile.emails[0].value.split('@')[0];
+                } else if (profile.name) {
+                    username = profile.name.givenName[0] + profile.name.familyName;
+                }
+
+                return username.toLowerCase() || undefined;
+            }
+        }
+    ));
 };
--- config/strategies/github.js
+++ config/strategies/github.js
@@ -24,10 +24,20 @@ module.exports = function() {
 			providerData.refreshToken = refreshToken;
 
 			// Create the user OAuth profile
+			var displayName = profile.displayName ? profile.displayName.trim() : profile.username.trim();
+			var iSpace = displayName.indexOf(' '); // index of the whitespace following the firstName
+			var firstName = iSpace !== -1 ? displayName.substring(0, iSpace) : displayName;
+			var lastName = iSpace !== -1 ? displayName.substring(iSpace + 1) : '';
+
 			var providerUserProfile = {
-				displayName: profile.displayName,
-				email: profile.emails[0].value,
+				firstName: firstName,
+				lastName: lastName,
+				displayName: displayName,
+				email: (profile.emails && profile.emails.length) ? profile.emails[0].value : undefined,
 				username: profile.username,
+				// jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+				profileImageURL: (providerData.avatar_url) ? providerData.avatar_url : undefined,
+				// jscs:enable
 				provider: 'github',
 				providerIdentifierField: 'id',
 				providerData: providerData
